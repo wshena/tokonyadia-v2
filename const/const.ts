135f04f80f6bd9aa37b18719feadbcc3c6a7d541
wshena <wshenaarrafi@gmail.com>
2026-03-14 20:58:39 +0700

    start:initialize project and install required library

---
+++ const/const.ts
@@ -0,0 +1,218 @@
+export const HomeCarouselContent = [
+  {
+    id: 1,
+    image: '/homeCarousel/item1.jpg'
+  },
+  {
+    id: 2,
+    image: '/homeCarousel/item2.jpg'
+  },
+  {
+    id: 3,
+    image: '/homeCarousel/item3.jpg'
+  },
+  {
+    id: 4,
+    image: '/homeCarousel/item.jpg.webp'
+  },
+]
+
+export const ProfileButtonLinks = [
+  {
+    id: 1,
+    label: 'pembelian',
+    link: '/order-list'
+  },
+  {
+    id: 2,
+    label: 'wishlist',
+    link: '/wishlist'
+  },
+  {
+    id: 3,
+    label: 'pengaturan',
+    link: '/user/settings'
+  },
+]
+
+export const FooterLinksContent = [
+  {
+    title: "Tokopedia",
+    links: [
+      { label: "Tentang Tokopedia", link: "#" },
+      { label: "Hak Kekayaan Intelektual", link: "#" },
+      { label: "Karir", link: "#" },
+      { label: "Blog", link: "#" },
+      { label: "Tokopedia Affiliate Program", link: "#" },
+      { label: "Tokopedia B2B Digital", link: "#" },
+      { label: "Tokopedia Marketing Solutions", link: "#" },
+      { label: "Kalkulator Indeks Masa Tubuh", link: "#" },
+      { label: "Tokopedia Farma", link: "#" },
+      { label: "Promo Hari Ini", link: "#" },
+      { label: "Beli Lokal", link: "#" },
+      { label: "Promo Guncang", link: "#" },
+    ]
+  },
+  {
+    title: "Beli",
+    links: [
+      { label: "Tagihan & Top Up", link: "#" },
+      { label: "Tokopedia COD", link: "#" },
+      { label: "Bebas Ongkir", link: "#" },
+    ]
+  },
+  {
+    title: "Jual",
+    links: [
+      { label: "Pusat Edukasi Seller", link: "#" },
+      { label: "Daftar Mall", link: "#" },
+    ]
+  },
+  {
+    title: "Bantuan dan Panduan",
+    links: [
+      { label: "Tokopedia Care", link: "#" },
+      { label: "Syarat dan Ketentuan", link: "#" },
+      { label: "Kebijakan Privasi", link: "#" },
+    ]
+  }
+];
+
+export const ContactFooter = [
+  {
+    label:'facebook',
+    link: 'https://www.facebook.com/',
+  },
+  {
+    label:'twitter',
+    link: 'https://www.twitter.com/',
+  },
+  {
+    label:'pinterest',
+    link: 'https://www.pinterest.com/',
+  },
+  {
+    label:'instagram',
+    link: 'https://www.instagram.com/',
+  },
+]
+
+export const MetodePengiriman = [
+  {
+    id:1,
+    label: 'Standard (USD 1.00)',
+    value: 'standard',
+  },
+  {
+    id:2,
+    label: 'Same Day 8 Jam (USD 5.00)',
+    value: 'same-day',
+  },
+  {
+    id:3,
+    label: 'Instant 3 Jam (USD 8.00)',
+    value: 'instant',
+  },
+  {
+    id:4,
+    label: 'Reguler (USD 1.00 - USD 5.00)',
+    value: 'reguler',
+  },
+]
+
+export const MetodePembayaran = [
+  {
+    id:1,
+    label: 'COD (Bayar Ditempat)',
+    value: 'cod',
+  },
+  {
+    id:2,
+    label: 'BRI Virtual Account',
+    value: 'brivia',
+  },
+  {
+    id:3,
+    label: 'Alfamart, Alfamidi, Lawson',
+    value: 'alfa-group',
+  },
+  {
+    id:4,
+    label: 'BCA Virtual Account',
+    value: 'bca-virtual',
+  },
+]
+
+export const EmptyWishlistContent = [
+  {
+    id:1,
+    title: 'Simpan barang impian',
+    paragraph: 'Mau simpan barang untuk beli nanti? Bandingkan harga dan spesifikasinya? Di sini tempatnya!',
+    image: '/image/1-emptystate.png'
+  },
+  {
+    id:2,
+    title: 'Klik icon hati buat simpan',
+    paragraph: 'Lagi lihat-lihat, ada barang yang kamu suka? Klik ikon hati buat simpan di Wishlist.',
+    image: '/image/2-emptystate.png'
+  },
+  {
+    id:3,
+    title: 'Atur Wishlist dengan Koleksi',
+    paragraph: 'Pakai fitur Koleksi buat mengelompokkan barang-barang di Wishlist sesukamu.',
+    image: '/image/3-emptystate.png'
+  },
+]
+
+export const UserSettings = [
+  {
+    id:1,
+    label: 'Biodata Diri',
+    link: '/user/settings'
+  },
+  {
+    id:2,
+    label: 'Daftar Alamat',
+    link: '/user/settings/address'
+  },
+  {
+    id:3,
+    label: 'Notifikasi',
+    link: '/user/settings/notification'
+  },
+]
+
+export const NotificationSettings = [
+  {
+    id:1,
+    title:'Transaksi Pemebelian',
+    settings: [
+      'menunggu pembayaran', 'menunggu konfirmasi', 'pesanan diproses', 'pesanan dikirim', 'pesanan selesai', 'pengingat'
+    ]
+  },
+  {
+    id:2,
+    title:'Promo',
+    settings: [
+      'newsletter tokonyadia'
+    ]
+  },
+]
+
+export const MobileNavLink = [
+  {
+    id:1,
+    label: 'Collections',
+    link: '/collection/all'
+  },
+  {
+    id:2,
+    label: 'Category',
+    link: '/category/all'
+  },
+  {
+    id:3,
+    label: 'Cart',
+    link: '/cart'
+  },
+]
---
+++ const/filterConst.ts
@@ -0,0 +1,32 @@
+export const ProductFilterDropdown = [
+  {
+    id: 1,
+    filter: 'paling sesuai',
+    value: '',
+    order: ''
+  },
+  {
+    id: 2,
+    filter: 'ulasan',
+    value: 'performance.ratingAverage',
+    order: 'desc'
+  },
+  {
+    id: 3,
+    filter: 'terlaris',
+    value: 'performance.sales',
+    order: 'desc'
+  },
+  {
+    id: 4,
+    filter: 'harga tertinggi',
+    value: 'price.withoutDiscount',
+    order: 'desc'
+  },
+  {
+    id: 5,
+    filter: 'harga terendah',
+    value: 'price.withoutDiscount',
+    order: 'asc'
+  },
+]
